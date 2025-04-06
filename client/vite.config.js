0408fa9e9c0298d7f412de5beba971340e4b5655
Rishi Rawat <rawatrishi390@gmail.com>
2025-04-06 20:11:51 +0530

    update file

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: './',  
   plugins: [react()],
   css: {
     postcss: false,
@@ -16,4 +17,4 @@ export default defineConfig({
     },
     host: '0.0.0.0',
   }
-}) 
+})
