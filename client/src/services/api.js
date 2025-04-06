8aabfd48d8a646234cc349b11d7eaf8da7935280
Rishi Rawat <rawatrishi390@gmail.com>
2025-04-06 16:16:37 +0530

    Update api.js

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5000/api';
+const API_URL = 'https://rishitech.shop/api';
 
 export const todoApi = {
   getAllTodos: () => axios.get(`${API_URL}/todos`),
